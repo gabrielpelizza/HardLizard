889afdf7d44613edfa0ca894289e59da2bbe5e9d
ivanrutz <ivanagustinrutz@gmail.com>
2020-11-18 13:07:39 -0300

    Agrego total de peliculas

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ let movies = homePage.leerJSON()
 
 module.exports = {
     homePage : function(req,res){
+        res.write('Bienvenidos a DH  Movies el mejor sitio para encontrar las mejores peliculas, incluso mucho mejor que Netflix, Cuevana y PopCorn. \n')
+       res.write(`Total de películas en cartelera: ${movies.total_movies} \n`)
+       
         movies.movies.forEach(movie => {
             res.write(movie.title +'\n')
         });
